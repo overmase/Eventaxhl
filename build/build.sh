2c9d1261000fe155b3233c8d2e5d7820e2f2e508
frago9876543210 <al.volf.av@gmail.com>
2017-04-21 19:22:10 +0300

    build

---
+++ build/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+PHP_BINARY="./bin/php7/bin/php"
+
+while getopts "p:" OPTION 2> /dev/null; do
+	case ${OPTION} in
+		p)
+			PHP_BINARY="$OPTARG"
+			;;
+	esac
+done
+
+cp -r tests/plugins plugins
+"$PHP_BINARY" ./plugins/PocketMine-DevTools/src/DevTools/ConsoleScript.php --make ./plugins/PocketMine-DevTools --relative ./plugins/PocketMine-DevTools --out ./plugins/DevTools.phar
+rm -rf ./plugins/PocketMine-DevTools
+
+echo -e "version\nmakeserver\nstop\n" | "$PHP_BINARY" src/pocketmine/PocketMine.php --no-wizard --disable-ansi --disable-readline --debug.level=2
+if ls plugins/DevTools/FastCore*.phar >/dev/null 2>&1; then
+    echo Server phar created successfully.
+else
+    echo No phar created!
+    exit 1
+fi
---
+++ build/update.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/ISRAPIL/FastCorePE.git; sleep 3; rm -r ./src; mv ./FastCorePE/src ./; rm -r ./FastCorePE
