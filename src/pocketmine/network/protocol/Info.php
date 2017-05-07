f0024718c810a15b0944dba8411d77a435485dab
frago9876543210 <al.volf.av@gmail.com>
2017-05-07 09:07:35 +0300

    update to 1.0.8

--- src/pocketmine/network/protocol/Info.php
+++ src/pocketmine/network/protocol/Info.php
@@ -34,9 +34,9 @@ interface Info
      */
 
     const CURRENT_PROTOCOL = 107;
-    const ACCEPTED_PROTOCOLS = [106, 107]; //106 need test
-    const MINECRAFT_VERSION = "v1.0.7";
-    const MINECRAFT_VERSION_NETWORK = "1.0.7";
+    const ACCEPTED_PROTOCOLS = [107];
+    const MINECRAFT_VERSION = "v1.0.8";
+    const MINECRAFT_VERSION_NETWORK = "1.0.8";
 
     const LOGIN_PACKET = 0x01;
     const PLAY_STATUS_PACKET = 0x02;
